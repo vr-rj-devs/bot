a908bf1958c236b55a96cdf9dba8c74501498b37
Felipe Ribeiro Lobato <felipelobato.rj@gmail.com>
2024-01-17 00:02:18 -0300

    feat: move env validation to proxied processEnv

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,14 +1,23 @@
 import z from "zod";
+import { generateErrorMessage } from "zod-error";
+import dotenv from "dotenv";
+dotenv.config();
 
 const envSchema = z.object({
   TOKEN: z.string(),
   CLIENT_ID: z.string(),
-  GUILD_ID: z.string()
+  GUILD_ID: z.string(),
 });
+const safeParseResult = envSchema.safeParse(process.env);
 
-export const ENV = envSchema.safeParse(process.env);
+export const ENV = new Proxy(process.env as typeof envSchema._type, {
+  get: (target, name) => {
+    if (!safeParseResult.success) {
+      console.error("❌ Invalid environment variables, check the errors below!");
+      console.error(generateErrorMessage(safeParseResult.error.issues, { delimiter: { error: "\\n" } }));
+      process.exit(-1);
+    }
 
-export const getEnvIssues = (): z.ZodIssue[] | void => {
-  const result = envSchema.safeParse(process.env);
-  if (!result.success) return result.error.issues;
-};
+    return target[name as keyof typeof target];
+  },
+});
